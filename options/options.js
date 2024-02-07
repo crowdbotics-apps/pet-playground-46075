8efc74c9a6097bb6123203d957b57bd12d4c5229
Crowdbotics <team@crowdbotics.com>
2024-02-07 16:46:43 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pet-playground-46075",
+  url: "https://pet-playground-46075.botics.co",
+  api: "https://pet-playground-46075.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
